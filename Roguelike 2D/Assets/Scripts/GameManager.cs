f1bcc03a3cae4e3d311480ac64af5ccf0ecd27c9
Sean Avila <sean.avila94@gmail.com>
2022-02-03 16:34:56 -0600

    Added player script
    -Added Player script which is a child of Moving Object
    - Added functionality to interact with walls, items and enemies

--- Roguelike 2D/Assets/Scripts/GameManager.cs
+++ Roguelike 2D/Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
 
     private int level = 4;
 
@@ -29,6 +31,11 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
     }
 
     // Update is called once per frame
