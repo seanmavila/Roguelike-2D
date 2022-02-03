08544dbdc4dc0b13a7861e435bb5dd49f084c62e
Sean Avila <sean.avila94@gmail.com>
2022-02-03 15:20:03 -0600

    Setup GameManager
    - GameManager script made into a singleton and sets the game up using the BoardManager script.
    - Loader script added to MainCamera that calls the GameManger script

--- Roguelike 2D/Assets/Scripts/GameManager.cs
+++ Roguelike 2D/Assets/Scripts/GameManager.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 4;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
